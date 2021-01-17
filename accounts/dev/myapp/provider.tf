deb90a9af524bbf3b627e57ce427947ee34b3e64
Jonas Neves <jneves@seniorlink.com>
2021-01-17 12:38:03 -0500

    Replace hardcoded value with var

--- accounts/dev/myapp/provider.tf
+++ accounts/dev/myapp/provider.tf
@@ -10,7 +10,7 @@ data "aws_caller_identity" "ops" {
 provider "aws" {
   region = local.vars.region
   assume_role {
-    role_arn     = "arn:aws:iam::413213983779:role/${local.vars.accounts.dev.role}"
+    role_arn     = "arn:aws:iam::${local.vars.accounts.dev.id}:role/${local.vars.accounts.dev.role}"
     session_name = split(":", data.aws_caller_identity.ops.user_id)[1]
   }
 }
