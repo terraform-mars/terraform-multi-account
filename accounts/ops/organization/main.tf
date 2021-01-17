420788592fe98306b66f2b5db72596f22bbeeaf9
Jonas Neves <jneves@seniorlink.com>
2021-01-17 12:55:16 -0500

    Add description to local "vars"

--- accounts/ops/organization/main.tf
+++ accounts/ops/organization/main.tf
@@ -6,6 +6,8 @@ locals {
   # Must be updated if you move this directory to another dir level
   relpath_to_root = "../../.."
   
+  # Look for shared vars in every dir level up to the repo root directory
+  # Variables in nearest files takes precedence
   vars = yamldecode(join("\n", [
     for dir in sort(compact([
       for vf in fileset(path.module, "${local.relpath_to_root}/**/vars.yml") :
